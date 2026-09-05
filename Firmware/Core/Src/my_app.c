2a79ccbf3f54ebaaffacc48043983d6d7a6dbed6
omarsweiss <swaiss.omar@gmail.com>
2026-09-05 18:56:53 +0300

    Parsing received data using struct.unpack

--- Firmware/Core/Src/my_app.c
+++ Firmware/Core/Src/my_app.c
@@ -1,16 +1,23 @@
 #include "my_app.h"
 #include "projdefs.h"
+#include "stm32g474xx.h"
 #include "usbd_def.h"
+#include <stdint.h>
+#include <string.h>
 
 
 void my_app(void);
 void vTask1(void* pvParameters);
-
-
 void vTask1(void* pvParameters){
-    
+
     for(;;){
-        CDC_Transmit_FS((uint8_t*) adcVal, sizeof(adcVal));
+        
+        taskENTER_CRITICAL();
+        while(CDC_Transmit_FS((uint8_t*) adcVal, sizeof(adcVal)) == USBD_BUSY){
+            flag++;
+        }
+ 
+        taskEXIT_CRITICAL();
         
     }
 }
@@ -22,7 +29,7 @@ void my_app(void){
     
     xTaskCreate(vTask1,
 "Task1",
-100,
+400,
 NULL,
 2,
 NULL);
--- pcHost/Receiver.py
+++ pcHost/Receiver.py
@@ -1,9 +1,33 @@
-from PyQt6 import QtWidgets, QtCore
+from PyQt6.QtWidgets import *
 import pyqtgraph as pg
 import sys
 import serial
-ser = serial.Serial('/dev/ttyACM1',timeout=1)
-while True:
-   data = ser.readline(400)
-   print(data)
+import struct
 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("title")
+        button = QPushButton("button")
+        
+        self.setFixedSize(400,400)
+        self.plot_graph = pg.PlotWidget()
+        self.setCentralWidget(self.plot_graph)
+        minutes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        temperature = [30, 32, 34, 32, 33, 31, 29, 32, 35, 30]
+        self.plot_graph.plot(minutes,temperature)
+ser = serial.Serial('/dev/ttyACM0',timeout=1)
+i=0
+readings = [0]*200
+while True:
+    while i!=200:
+        readings[i] = ser.readline(2)
+        i=i+1
+    i=0
+    
+    unpacked_list = [struct.unpack('<H', chunk)[0] for chunk in readings] 
+    print(unpacked_list)
+app = QApplication(sys.argv) #argv could be which serial port
+window = MainWindow()
+window.show()
+app.exec()
